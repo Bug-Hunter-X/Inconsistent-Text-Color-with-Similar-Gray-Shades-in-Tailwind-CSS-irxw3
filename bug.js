5bfe791370762fe659e2afb6ea2745089072cae9
BugHunter-X <askzenai@gmail.com>
2025-02-24 18:14:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ... some Tailwind CSS classes ...
+<div class="bg-gray-100 p-4">
+  <p class="text-lg font-bold">This text should be dark gray</p>
+</div>
+```
+
+This results in dark gray text on a light gray background, which is not readable.
